acd66347d80f1354236761394baa2e1bba4cfe48
flyingbucket <menglinli598@gmail.com>
2025-12-20 23:10:24 +0800

    update

---
+++ sync.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+VAULTS=~/Documents/vaults
+DEST=./content
+
+rsync -av --delete \
+  --exclude '.git' \
+  --exclude '.obsidian' \
+  --exclude 'StableSR_doc' \
+  --exclude '.gitignore' \
+  --exclude '.vscode' \
+  --exclude '.html' \
+  $VAULTS/* $DEST/
